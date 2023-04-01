95f842477d0617881b6a89540cc3996938ea7ab8
Abduvaliyev Alisher <94097356+Alisher003@users.noreply.github.com>
2023-03-31 23:27:33 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,45 +5,32 @@ let elFormBox = document.querySelector(".form-box");
 let elCloseBtn = document.querySelector(".close-btn");
 let elAddBtn = document.querySelector(".add-btn");
 let elModal = document.querySelector(".modal");
-// let elModalTitle = document.querySelector(".book-title");
-// let elModalAuther = document.querySelector(".book-auther");
-// let elModalPage = document.querySelector(".book-pages");
-// let elModalDate = document.querySelector(".book-date");
-let elTemplate = document.querySelector("#book-tem").content
-let elBookList = document.querySelector(".book-list")
-let elModalSelect = document.querySelector("#modal-select")
+let elTemplate = document.querySelector("#book-tem").content;
+let elBookList = document.querySelector(".book-list");
+let elModalSelect = document.querySelector("#modal-select");
 let elForm = document.querySelector(".moda-form");
 let elTitleInput = document.querySelector(".modal-title");
 let elAuthorInput = document.querySelector(".modal-auther");
 let elPagesInput = document.querySelector(".modal-pages");
 let eldateInput = document.querySelector(".modal-date");
 
-
 // Login
-elLoginBtn.addEventListener("click" , () => {
-  elFormBox.classList.replace("hide" , "show")
-})
+elLoginBtn.addEventListener("click", () => {
+  elFormBox.classList.replace("hide", "show");
+});
 
-elCloseBtn.addEventListener("click" , () => {
-  elFormBox.classList.replace("show" , "hide")
-})
+elCloseBtn.addEventListener("click", () => {
+  elFormBox.classList.replace("show", "hide");
+});
 
 // form
-elAddBtn.addEventListener("click" , () => {
-console.log("hi");
+elAddBtn.addEventListener("click", () => {
   elAddBtn.style.display = "none";
-  elModal.classList.toggle( "show" )
-})
-
+  elModal.classList.toggle("show");
+});
 
 const storedBooks = localStorage.getItem("books");
 const Books = storedBooks ? JSON.parse(storedBooks) : [];
-
-// let elForm = document.querySelector(".moda-form");
-// let elTitleInput = document.querySelector(".modal-title");
-// let elAuthorInput = document.querySelector(".modal-auther");
-// let elPagesInput = document.querySelector(".modal-pages");
-// let eldateInput = document.querySelector(".modal-date");
 
 elForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -54,63 +41,61 @@ elForm.addEventListener("submit", (event) => {
     pages: elPagesInput.value,
     date: eldateInput.value,
     id: Books[Books.length - 1]?.id + 1 || 0,
-    status: elModalSelect.value
+    status: elModalSelect.value,
   };
-
 
   Books.push(newBook);
   localStorage.setItem("books", JSON.stringify(Books));
-  render(Books , elBookList)
-
+  render(Books, elBookList);
 
   elModal.classList.replace("show", "hide");
   elAddBtn.style.display = "block";
 
-
+  elTitleInput.value = " ";
+  elAuthorInput.value = " ";
+  elPagesInput.value = " ";
+  eldateInput.value = " ";
 });
 
 
-function render(arr , list) {
-  list.innerHTML = null
-  const  fragment = document.createDocumentFragment();
+function render(arr, list) {
+  list.innerHTML = null;
+  const fragment = document.createDocumentFragment();
 
+  arr.forEach((book) => {
+    const elTemplateCopy = elTemplate.cloneNode(true);
+    const elBookItemTitle = elTemplateCopy.querySelector(".book-item__title");
+    const elBookItemAuther = elTemplateCopy.querySelector(".book-item__author");
+    const elBookItemPage = elTemplateCopy.querySelector(".book-item__page");
+    const elBookItemDate = elTemplateCopy.querySelector(".book-item__date");
+    const elBookItemDalete = elTemplateCopy.querySelector(".book-item__dalete");
+    const elBookItemCheck = elTemplateCopy.querySelector(".book-item__check");
 
-  arr.forEach(book => {
-    const elTemplateCopy = elTemplate.cloneNode(true)
-      const elBookItemTitle = elTemplateCopy.querySelector(".book-item__title");
-      const elBookItemAuther = elTemplateCopy.querySelector(".book-item__author");
-      const elBookItemPage = elTemplateCopy.querySelector(".book-item__page");
-      const elBookItemDate = elTemplateCopy.querySelector(".book-item__date");
-      const elBookItemDalete = elTemplateCopy.querySelector(".book-item__dalete");
-      const elBookItemCheck = elTemplateCopy.querySelector(".book-item__check");
+    elBookItemTitle.textContent = book.title;
+    elBookItemAuther.textContent = book.author;
+    elBookItemPage.textContent = book.pages;
+    elBookItemDate.textContent = book.date;
+    elBookItemDalete.dataset.bookId = book.id;
+    elBookItemCheck.textContent = book.status;
 
-      elBookItemTitle.textContent = book.title
-      elBookItemAuther.textContent = book.author
-      elBookItemPage.textContent = book.pages
-      elBookItemDate.textContent = book.date
-      elBookItemDalete.dataset.bookId = book.id
-      elBookItemCheck.textContent = book.status
-
-      fragment.appendChild(elTemplateCopy);
-
+    fragment.appendChild(elTemplateCopy);
   });
-  list.appendChild(fragment)
-
+  list.appendChild(fragment);
 }
 
-
-elBookList.addEventListener("click" , (evt) => {
-  evt.preventDefault()
-  if(evt.target.matches(".book-item__dalete")){
-    const index = Books.findIndex((book) => book.id == evt.target.dataset.bookId)
-    Books.splice(index,1)
-    for(let i = index; i<Books.length; i++){
-      Books[i].id--
+elBookList.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  if (evt.target.matches(".book-item__dalete")) {
+    const index = Books.findIndex(
+      (book) => book.id == evt.target.dataset.bookId
+    );
+    Books.splice(index, 1);
+    for (let i = index; i < Books.length; i++) {
+      Books[i].id--;
     }
     localStorage.setItem("books", JSON.stringify(Books));
-    render(Books, elBookList)
+    render(Books, elBookList);
   }
-})
+});
 
-
-render(Books, elBookList)
+render(Books, elBookList);
